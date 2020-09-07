2599f39f1d94b485fdd45d4dfd2aa2bcd3c967d4
M Aghead Albakhe <aghyadalbalkhi@gmail.com>
2020-09-07 13:22:21 +0300

    adding js files and josn eslintrc

---
+++ classes/class-02/js/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+var answer = prompt('Do You Want some Break ?');
+console.log(answer);
+
+var test =answer;
+switch (test) {
+
+    case 10:
+        alert('do what u want 10');
+        break;
+
+        case 'true':
+        alert('do what u want');
+        break;
+
+        case 'Cadilac':
+        alert('do what u want');
+        break;
+
+
+
+    default:
+        alert('your car not here 55');
+        break;
+}
